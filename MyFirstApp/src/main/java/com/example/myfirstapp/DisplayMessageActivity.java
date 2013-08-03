805ec1c50153c1f8c863beb95a14005de2e59cb1
Madhav <madhav.ramkumar@gmail.com>
2013-08-03 16:20:38 +0530

    Lesson: 'Starting Another Activity'' completed

--- MyFirstApp/src/main/java/com/example/myfirstapp/DisplayMessageActivity.java
+++ MyFirstApp/src/main/java/com/example/myfirstapp/DisplayMessageActivity.java
@@ -2,10 +2,12 @@ package com.example.myfirstapp;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 /**
  * Created by Banumathi on 8/3/13.
@@ -16,7 +18,17 @@ public class DisplayMessageActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_display_message);
+
+        // Get the message from the intent
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        //Create the text view
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+
+        setContentView(textView);
 
         //Make sure we are running on Honeycomb or Higher to use ActionBar APIs
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB){
--- MyFirstApp/src/main/java/com/example/myfirstapp/MainActivity.java
+++ MyFirstApp/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends Activity {
 
     /** Called when the user clicks the Send button */
     public void sendMessage(View view){
-        //ToDo Something in response to button
+        //Something in response to button
         Intent intent = new Intent(this, DisplayMessageActivity.class);
         EditText editText = (EditText) findViewById(R.id.edit_message);
         String message = editText.getText().toString();
